fe6e6dd629e1e0ab51ed05962917b23f4e67be27
Maria José Solano <majosolano99@gmail.com>
2022-04-10 18:57:30 -0700

    Separate animators

---
+++ Assets/Scripts/Core/RandomItemSpawner.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Raton.Core
+{
+    public class RandomItemSpawner : MonoBehaviour
+    {
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
